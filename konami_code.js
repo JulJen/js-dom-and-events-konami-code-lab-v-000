6b9015788bf68efe107532f3fe1ab59f370d4d95
Jul Jen <juliajen.prog@gmail.com>
2018-10-10 16:26:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,14 +1,14 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+// Keep track of index outside of the event handler.
+let index = 0;
+
+
 function init() {
   // Write your JavaScript code inside the init() function
 
 }
 
-
-
-// Keep track of index outside of the event handler.
-let index = 0;
 
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
